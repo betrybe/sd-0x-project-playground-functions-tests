ac60ed231f0107c38f11fbc0b09f7768563c7f07
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-07-16 12:57:01 -0300

    CR suggestions

--- tests/dateCheck.spec.js
+++ tests/dateCheck.spec.js
@@ -2,20 +2,20 @@ const challenges = require('../src/challenges');
 
 describe('Analisando as datas', () => {
   it('Se não possível, em nenhuma ordem, formar uma data, deve-se retornar "números inválidos"', () => {
-    expect(challenges.dateCheck(13, 24, 42)).toEqual('números inválidos');
-    expect(challenges.dateCheck(45, 64, 21)).toEqual('números inválidos');
-    expect(challenges.dateCheck(13, 14, 51)).toEqual('números inválidos');
+    expect(challenges.dateCheck(13, 24, 42)).toBe('números inválidos');
+    expect(challenges.dateCheck(45, 64, 21)).toBe('números inválidos');
+    expect(challenges.dateCheck(13, 14, 51)).toBe('números inválidos');
   });
 
   it('Se os valores passados formarem diversas datas, deve-se retornar a string "múltiplas datas"', () => {
-    expect(challenges.dateCheck(10, 12, 99)).toEqual('múltiplas datas');
-    expect(challenges.dateCheck(5, 8, 20)).toEqual('múltiplas datas');
-    expect(challenges.dateCheck(12, 6, 10)).toEqual('múltiplas datas');
+    expect(challenges.dateCheck(10, 12, 99)).toBe('múltiplas datas');
+    expect(challenges.dateCheck(05, 08, 20)).toBe('múltiplas datas');
+    expect(challenges.dateCheck(12, 06, 10)).toBe('múltiplas datas');
   });
 
   it('Se os números podem formar apenas uma data, deve-se retornar uma string no formato "DD/MM/AA"', () => {
-    expect(challenges.dateCheck(65, 12, 30)).toEqual('30/12/65');
-    expect(challenges.dateCheck(39, 24, 7)).toEqual('24/7/39A');
-    expect(challenges.dateCheck(6, 13, 81)).toEqual('13/6/81');
+    expect(challenges.dateCheck(65, 12, 30)).toBe('30/12/65');
+    expect(challenges.dateCheck(39, 24, 07)).toBe('24/07/39');
+    expect(challenges.dateCheck(06, 13, 81)).toBe('13/06/81');
   });
 });
--- tests/encodeDecode.spec.js
+++ tests/encodeDecode.spec.js
@@ -5,13 +5,13 @@ describe('Codifique e Decodifique', () => {
     expect(challenges.encode('hello').toEqual('h2ll4'));
     expect(challenges.encode('How are you today?').toEqual('H4w 1r2 y45 t4d1y?'));
     expect(challenges.encode('This is an encoding test.').toEqual('Th3s 3s 1n 2nc4d3ng t2st.'));
-    expect(challenges.encode('go Trybe!').toEqual('g4 Tryb2'));
+    expect(challenges.encode('go Trybe!').toEqual('g4 Tryb2!'));
   });
 
   it('Ao receber uma string codificada a função decode deve decodigicá-la', () => {
     expect(challenges.decode('h2ll4').toEqual('hello'));
     expect(challenges.decode('H4w 1r2 y45 t4d1y?').toEqual('How are you today?'));
     expect(challenges.decode('Th3s 3s 1n 2nc4d3ng t2st.').toEqual('This is an encoding test.'));
-    expect(challenges.decode('g4 Tryb2').toEqual('go Trybe!'));
+    expect(challenges.decode('g4 Tryb2!').toEqual('go Trybe!'));
   });
 });
